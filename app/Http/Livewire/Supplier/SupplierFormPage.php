06ef3e67f252fe40b6c109d2d8473afa16cfa140
chagy <chagy_x_@hotmail.com>
2021-12-20 20:52:24 +0700

    Supplier Validate

--- app/Http/Livewire/Supplier/SupplierFormPage.php
+++ app/Http/Livewire/Supplier/SupplierFormPage.php
@@ -6,6 +6,46 @@ use Livewire\Component;
 
 class SupplierFormPage extends Component
 {
+    public $sup_name;
+    public $sup_tax_number;
+    public $sup_email;
+    public $sup_phone;
+    public $sup_address;
+    public $province_id;
+    public $district_id;
+    public $sub_district_id;
+    public $sup_zip_code;
+    public $sup_contact_name;
+    public $sup_contact_phone;
+    public $sup_status=1;
+
+    protected $rules = [
+        'sup_name' => 'required',
+        'sup_address' => 'required',
+        'province_id' => 'required',
+        'district_id' => 'required',
+        'sub_district_id' => 'required',
+        'sup_zip_code' => 'required',
+        'sup_contact_name' => 'required',
+        'sup_contact_phone' => 'required',
+    ];
+
+    protected $messages = [
+        'sup_name.required' => 'กรุณากรอกชื่อบริษัท',
+        'sup_address.required' => 'กรุณากรอกที่อยู่',
+        'province_id.required' => 'กรุณาเลือกจังหวัด',
+        'district_id.required' => 'กรุณาเลือกอำเภอ',
+        'sub_district_id.required' => 'กรุณาเลือกตำบล',
+        'sup_zip_code.required' => 'กรุณากรอกรหัสไปรษณีย์',
+        'sup_contact_name.required' => 'กรุณากรอกชื่อผู้ติดต่อ',
+        'sup_contact_phone.required' => 'กรุณากรอกเบอร์โทรผู้ติดต่อ',
+    ];
+
+    public function save()
+    {
+        $this->validate($this->rules,$this->messages);
+    }
+
     public function render()
     {
         return view('livewire.supplier.supplier-form-page')->extends('layouts.main');
--- resources/views/livewire/supplier/supplier-form-page.blade.php
+++ resources/views/livewire/supplier/supplier-form-page.blade.php
@@ -8,152 +8,194 @@
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="sup_name">ชื่อบริษัท</label>
-                                <input 
-                                    type="text" 
-                                    class="form-control" 
-                                    name="sup_name"
-                                    id="sup_name" 
-                                    placeholder="ชื่อบริษัท" />
+                    <form wire:submit.prevent="save">
+                        <div class="row">
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="sup_name">ชื่อบริษัท</label>
+                                    <input 
+                                        type="text" 
+                                        class="form-control @error('sup_name') is-invalid @enderror" 
+                                        name="sup_name"
+                                        id="sup_name" 
+                                        placeholder="ชื่อบริษัท" />
+                                    @error('sup_name')
+                                    <div id="sup_name_validation" class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="sup_tax_number">เลขประจำตัวผู้เสียภาษี</label>
+                                    <input 
+                                        type="text" 
+                                        class="form-control" 
+                                        name="sup_tax_number"
+                                        id="sup_tax_number" 
+                                        placeholder="เลขประจำตัวผู้เสียภาษี" />
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="sup_email">อีเมล์</label>
+                                    <input 
+                                        type="text" 
+                                        class="form-control" 
+                                        name="sup_email"
+                                        id="sup_email" 
+                                        placeholder="อีเมล์" />
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="sup_phone">เบอร์โทร</label>
+                                    <input 
+                                        type="text" 
+                                        class="form-control" 
+                                        name="sup_phone"
+                                        id="sup_phone" 
+                                        placeholder="เบอร์โทร" />
+                                </div>
                             </div>
                         </div>
-                        <div class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="sup_tax_number">เลขประจำตัวผู้เสียภาษี</label>
-                                <input 
-                                    type="text" 
-                                    class="form-control" 
-                                    name="sup_tax_number"
-                                    id="sup_tax_number" 
-                                    placeholder="เลขประจำตัวผู้เสียภาษี" />
+                        <div class="row">
+                            <div class="col-12 col-md-12">
+                                <div class="form-group">
+                                    <label for="sup_address">ที่อยู่</label>
+                                    <input 
+                                        type="text" 
+                                        class="form-control @error('sup_address') is-invalid @enderror" 
+                                        name="sup_address"
+                                        id="sup_address" 
+                                        placeholder="ที่อยู่" />
+                                    @error('sup_address')
+                                    <div id="sup_address_validation" class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="province_id">จังหวัด</label>
+                                    <select 
+                                        class="form-control @error('province_id') is-invalid @enderror" 
+                                        name="province_id"
+                                        id="province_id" >
+                                        <option value="">-- เลือกจังหวัด --</option>
+                                    </select>
+                                    @error('province_id')
+                                    <div id="province_id_validation" class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="district_id">อำเภอ</label>
+                                    <select 
+                                        class="form-control @error('district_id') is-invalid @enderror" 
+                                        name="district_id"
+                                        id="district_id" >
+                                        <option value="">-- เลือกอำเภอ --</option>
+                                    </select>
+                                    @error('district_id')
+                                    <div id="district_id_validation" class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="sub_district_id">ตำบล</label>
+                                    <select 
+                                        class="form-control @error('sub_district_id') is-invalid @enderror" 
+                                        name="sub_district_id"
+                                        id="sub_district_id" >
+                                        <option value="">-- เลือกตำบล --</option>
+                                    </select>
+                                    @error('sub_district_id')
+                                    <div id="sub_district_id_validation" class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="sup_zip_code">รหัสไปรษณีย์</label>
+                                    <input 
+                                        type="text" 
+                                        class="form-control @error('sup_zip_code') is-invalid @enderror" 
+                                        name="sup_zip_code"
+                                        id="sup_zip_code" 
+                                        placeholder="รหัสไปรษณีย์" />
+                                    @error('sup_zip_code')
+                                    <div id="sup_zip_code_validation" class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
                             </div>
                         </div>
-                        <div class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="sup_email">อีเมล์</label>
-                                <input 
-                                    type="text" 
-                                    class="form-control" 
-                                    name="sup_email"
-                                    id="sup_email" 
-                                    placeholder="อีเมล์" />
+                        <div class="row">
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="sup_contact_name">ชื่อผู้ติดต่อ</label>
+                                    <input 
+                                        type="text" 
+                                        class="form-control @error('sup_contact_name') is-invalid @enderror" 
+                                        name="sup_contact_name"
+                                        id="sup_contact_name" 
+                                        placeholder="ชื่อผู้ติดต่อ" />
+                                    @error('sup_contact_name')
+                                    <div id="sup_contact_name_validation" class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="sup_contact_phone">เบอร์โทรผู้ติดต่อ</label>
+                                    <input 
+                                        type="text" 
+                                        class="form-control @error('sup_contact_phone') is-invalid @enderror" 
+                                        name="sup_contact_phone"
+                                        id="sup_contact_phone" 
+                                        placeholder="เบอร์โทรผู้ติดต่อ" />
+                                    @error('sup_contact_phone')
+                                    <div id="sup_contact_phone_validation" class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
                             </div>
                         </div>
-                        <div class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="sup_phone">เบอร์โทร</label>
-                                <input 
-                                    type="text" 
-                                    class="form-control" 
-                                    name="sup_phone"
-                                    id="sup_phone" 
-                                    placeholder="เบอร์โทร" />
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label for="sup_status">สถานะ</label>
+                                    <select 
+                                        class="form-control" 
+                                        name="sup_status"
+                                        id="sup_status" >
+                                        <option value="1">ใช้งาน</option>
+                                        <option value="0">ยกเลิก</option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12 col-md-12">
-                            <div class="form-group">
-                                <label for="sup_address">ที่อยู่</label>
-                                <input 
-                                    type="text" 
-                                    class="form-control" 
-                                    name="sup_address"
-                                    id="sup_address" 
-                                    placeholder="ที่อยู่" />
-                            </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-block btn-primary">
+                                บันทึกข้อมูล
+                            </button>
                         </div>
-                        <div class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="province_id">จังหวัด</label>
-                                <select 
-                                    class="form-control" 
-                                    name="province_id"
-                                    id="province_id" >
-                                    <option value="">-- เลือกจังหวัด --</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="district_id">อำเภอ</label>
-                                <select 
-                                    class="form-control" 
-                                    name="district_id"
-                                    id="district_id" >
-                                    <option value="">-- เลือกอำเภอ --</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="sub_district_id">ตำบล</label>
-                                <select 
-                                    class="form-control" 
-                                    name="sub_district_id"
-                                    id="sub_district_id" >
-                                    <option value="">-- เลือกตำบล --</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="sup_zip_code">รหัสไปรษณีย์</label>
-                                <input 
-                                    type="text" 
-                                    class="form-control" 
-                                    name="sup_zip_code"
-                                    id="sup_zip_code" 
-                                    placeholder="รหัสไปรษณีย์" />
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="sup_contact_name">ชื่อผู้ติดต่อ</label>
-                                <input 
-                                    type="text" 
-                                    class="form-control" 
-                                    name="sup_contact_name"
-                                    id="sup_contact_name" 
-                                    placeholder="ชื่อผู้ติดต่อ" />
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="sup_contact_phone">เบอร์โทรผู้ติดต่อ</label>
-                                <input 
-                                    type="text" 
-                                    class="form-control" 
-                                    name="sup_contact_phone"
-                                    id="sup_contact_phone" 
-                                    placeholder="เบอร์โทรผู้ติดต่อ" />
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="form-group">
-                                <label for="sup_status">สถานะ</label>
-                                <select 
-                                    class="form-control" 
-                                    name="sup_status"
-                                    id="sup_status" >
-                                    <option value="1">ใช้งาน</option>
-                                    <option value="0">ยกเลิก</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-block btn-primary">
-                            บันทึกข้อมูล
-                        </button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
