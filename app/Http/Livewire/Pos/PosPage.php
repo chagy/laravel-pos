ded9d8ef651e3a9d6a1ea9cfa79f1b96ca1de2bd
chagy <chagy_x_@hotmail.com>
2022-01-13 20:41:09 +0700

    Pos BoxSearch

--- app/Http/Livewire/Pos/PosPage.php
+++ app/Http/Livewire/Pos/PosPage.php
@@ -3,11 +3,17 @@
 namespace App\Http\Livewire\Pos;
 
 use Livewire\Component;
+use App\Models\Category;
 
 class PosPage extends Component
 {
+    public $searchTerm;
+    public $category;
+
     public function render()
     {
-        return view('livewire.pos.pos-page')->extends('layouts.main');
+        return view('livewire.pos.pos-page',[
+            'categories' => Category::where('cate_status',1)->get()
+        ])->extends('layouts.main');
     }
 }
--- resources/views/livewire/pos/pos-page.blade.php
+++ resources/views/livewire/pos/pos-page.blade.php
@@ -3,6 +3,34 @@
     <div class="row">
         <div class="col-8">
             <div class="card">
+                <div class="card-body">
+                    <form>
+                        <div class="row">
+                            <div class="col-4">
+                                <select class="form-control" wire:model="category">
+                                    <option value="">ทั้งหมด</option>
+                                    @foreach ($categories as $cate)
+                                    <option value="{{ $cate->id }}">{{ $cate->cate_name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-8">
+                                <div class="input-group">
+                                    <input 
+                                        type="text" 
+                                        placeholder="ค้นหา ชื่อ/บาร์โค้ด" 
+                                        class="form-control" 
+                                        wire:model="searchTerm">
+                                    <span class="input-group-append">
+                                        <button class="btn btn-success">
+                                            <i class="fas fa-search"></i>
+                                        </button>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
             
             <div class="row mt-2 overflow-auto" style="min-height:60%;">
