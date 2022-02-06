4c1c8cf5be84ec2198576dcdfcdd3c6b11d75227
chagy <chagy_x_@hotmail.com>
2022-02-06 17:07:48 +0700

    Pos Save Fix Bug

--- app/Http/Livewire/Pos/PosProductEdit.php
+++ app/Http/Livewire/Pos/PosProductEdit.php
@@ -8,6 +8,7 @@ use Cart;
 class PosProductEdit extends Component
 {
     public $productId=0;
+    public $unit="";
     public $quantity=0;
     public $price=0;
     public $discountItem=0;
@@ -24,6 +25,7 @@ class PosProductEdit extends Component
         $this->quantity = $product->quantity;
         $this->price = $product->price;
         $this->discountItem = $product->attributes->psod_item_discount;
+        $this->unit = $product->attributes->psod_item_unit;
     }
 
     public function save()
@@ -36,6 +38,7 @@ class PosProductEdit extends Component
             'price' => $this->price,
             'attributes' => [
                 'dateOrder' => date('YmdHis'),
+                'psod_item_unit' => $this->unit,
                 'psod_item_price' => $this->price,
                 'psod_item_discount' => $this->discountItem,
                 'psod_item_discount_total' => $this->discountItem * $this->quantity,
