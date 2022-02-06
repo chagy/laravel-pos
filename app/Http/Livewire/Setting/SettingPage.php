1ffb38d5c9c658b6449c6f5642429a041c6b03b5
chagy <chagy_x_@hotmail.com>
2022-02-06 19:01:53 +0700

    Setting Form

--- app/Http/Livewire/Setting/SettingPage.php
+++ app/Http/Livewire/Setting/SettingPage.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class SettingPage extends Component
 {
+    public $sett_name;
+    public $sett_phone;
+    public $sett_tax_id;
+    public $sett_vat;
+    public $sett_owner;
+
     public function render()
     {
         return view('livewire.setting.setting-page')->extends('layouts.main');
--- resources/views/livewire/setting/setting-page.blade.php
+++ resources/views/livewire/setting/setting-page.blade.php
@@ -1,3 +1,109 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <br/>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">ฟอร์มตั้งค่า</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <form wire:submit.prevent="save">
+                        <div class="row">
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="sett_name">ชื่อร้าน</label>
+                                    <input 
+                                        type="text" 
+                                        class="form-control @error('sett_name') is-invalid @enderror" 
+                                        name="sett_name"
+                                        id="sett_name" 
+                                        placeholder="ชื่อร้าน" 
+                                        wire:model="sett_name"/>
+                                    @error('sett_name')
+                                    <div id="sett_name_validation" class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="sett_phone">เบอร์โทร</label>
+                                    <input 
+                                        type="text" 
+                                        class="form-control" 
+                                        name="sett_phone"
+                                        id="sett_phone" 
+                                        placeholder="เบอร์โทร" 
+                                        wire:model="sett_phone"/>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="sett_tax_id">เลขที่เสียภาษี</label>
+                                    <input 
+                                        type="text" 
+                                        class="form-control @error('sett_tax_id') is-invalid @enderror" 
+                                        name="sett_tax_id"
+                                        id="sett_tax_id" 
+                                        placeholder="เลขที่เสียภาษี" 
+                                        wire:model="sett_tax_id"/>
+                                    @error('sett_tax_id')
+                                    <div id="sett_tax_id_validation" class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="sett_vat">Vat %</label>
+                                    <input 
+                                        type="text" 
+                                        class="form-control @error('sett_vat') is-invalid @enderror" 
+                                        name="sett_vat"
+                                        id="sett_vat" 
+                                        placeholder="Vat %" 
+                                        wire:model="sett_vat"/>
+                                    @error('sett_vat')
+                                    <div id="sett_vat_validation" class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="sett_owner">ชื่อเจ้าของร้าน</label>
+                                    <input 
+                                        type="text" 
+                                        class="form-control @error('sett_owner') is-invalid @enderror" 
+                                        name="sett_owner"
+                                        id="sett_owner" 
+                                        placeholder="ชื่อเจ้าของร้าน" 
+                                        wire:model="sett_owner"/>
+                                    @error('sett_owner')
+                                    <div id="sett_owner_validation" class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-block btn-primary">
+                                บันทึกข้อมูล
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <div wire:loading wire:target="save"  wire:loading.class="overlay" wire:loading.flex>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <i class="fas fa-2x fa-sync fa-spin"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
