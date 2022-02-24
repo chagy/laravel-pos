70ca7383a6ca9c453f37fe8f9fa734a1f90bd6a5
chagy <chagy_x_@hotmail.com>
2022-02-24 21:49:31 +0700

    Pos print slip header name

--- app/Http/Controllers/PosPrintController.php
+++ app/Http/Controllers/PosPrintController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Helper;
+use App\Models\Setting;
+use Illuminate\Http\Request;
 
 class PosPrintController extends Controller
 {
     
     public function printSlip($id)
     {
+        $setting = Setting::orderBy('id','asc')->first();
+
         $mpdf = Helper::mpdf([100,1500],16);
-        $mpdf->WriteHTML(view('pos-print.slip'));
+        $mpdf->WriteHTML(view('pos-print.slip',[
+            'setting' => $setting
+        ]));
         $mpdf->Output('pos-order-'.date('d-m-Y').".pdf",'I');
         exit;
     }
--- resources/views/pos-print/slip.blade.php
+++ resources/views/pos-print/slip.blade.php
@@ -4,9 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Slip</title>
+
+    <style>
+        body {
+            font-size: 80%;
+        }
+        
+        @page {
+            margin: 2%;
+            margin-top: 3mm;
+            margin-header: 0mm;
+            margin-footer: 0mm;
+        }
+    </style>
 </head>
 <body>
-    <h1>สวัสดี</h1>
+    <div style="text-align: center;font-size: 2rem;line-height: 25px;">
+        ร้าน {{ $setting->sett_name }}
+    </div>
+    <div style="text-align: center;font-size: 2rem;line-height: 25px;">
+        เบอร์โทร : {{ $setting->sett_phone }}
+    </div>
 </body>
 </html>
