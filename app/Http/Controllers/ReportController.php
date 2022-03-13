21007276f7ba27ba088a7621097c4e6a2e303819
chagy <chagy_x_@hotmail.com>
2022-03-13 14:24:08 +0700

    Report day query data

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,12 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ReportController extends Controller
 {
     public function day(Request $request)
     {
-        return view('report.day');
+        $billNo = $request->billNo;
+        $employee = $request->employee;
+        
+        $dateEnd = $request->dateEnd ? $request->dateEnd : date("Y-m-d");
+        $dateStart = $request->dateStart ? $request->dateStart : date("Y-m-d",strtotime("-1 month",strtotime($dateEnd)));
+
+        $employees = User::where('type',1)->get();
+
+        $data  = DB::table('pos_orders')
+                    ->select(
+                        'pos_orders.created_at',
+                        'pos_orders.id',
+                        'users.name',
+                        'pos_orders.psod_qty',
+                        'pos_orders.psod_net_total'
+                    )
+                    ->join('users','users.id','=','pos_orders.customer_id')
+                    ->where('pos_orders.psod_status',1);
+        if($billNo){
+            $data = $data 
+                        ->where('pos_orders.id',$billNo);
+        }
+
+        if($employee){
+            $data = $data 
+                        ->where('pos_orders.employee_id',$employee);
+        }
+
+        if($dateStart && $dateEnd){
+            $data = $data 
+                        ->whereBetween('pos_orders.created_at',[$dateStart,$dateEnd]);
+        }
+
+        $data = $data 
+                    ->get();
+        
+
+        return view('report.day',[
+            'billNo' => $billNo,
+            'employee' => $employee,
+            'dateEnd' => $dateEnd,
+            'dateStart' => $dateStart,
+            'employees' => $employees,
+            'data' => $data
+        ]);
     }
 }
--- resources/views/report/day.blade.php
+++ resources/views/report/day.blade.php
@@ -32,6 +32,9 @@
                                             id="employee"
                                             class="form-control">
                                             <option value="">เลือกพนักงาน</option>
+                                            @foreach ($employees as $emp)
+                                            <option value="{{ $emp->id }}">{{ $emp->name }}</option>  
+                                            @endforeach
                                         </select>
                                     </div>
                                 </div>
@@ -78,6 +81,49 @@
                                     </div>
                                 </div>
                             </form>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>วันที่ เวลา</th>
+                                        <th>เลขที่ใบเสร็จ</th>
+                                        <th>ลูกค้า</th>
+                                        <th>จำนวนรายการ</th>
+                                        <th>จำนวนเงิน</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                        $qty = 0;
+                                        $total = 0;
+                                    @endphp
+                                    @foreach ($data as $item)
+                                    <tr>
+                                        <td class="text-center">{{ $loop->index+1 }}</td>
+                                        <td class="text-center">{{ date('d/m/y H:i',strtotime($item->created_at)) }}</td>
+                                        <td class="text-center">{{ $item->id }}</td>
+                                        <td>{{ $item->name }}</td>
+                                        <td class="text-right">{{ $item->psod_qty }}</td>
+                                        <td class="text-right">{{ number_format($item->psod_net_total,2) }}</td>
+                                    </tr>   
+                                    @php
+                                        $qty += $item->psod_qty;
+                                        $total += $item->psod_net_total;
+                                    @endphp
+                                    @endforeach
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <td class="text-center" colspan="4">ยอดรวม</td>
+                                        <td class="text-right">{{ number_format($qty,2) }}</td>
+                                        <td class="text-right">{{ number_format($total,2) }}</td>
+                                    </tr>
+                                </tfoot>
+                            </table>
                         </div>
                     </div>
                 </div>
