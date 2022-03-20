221972d1ec4410354a662e77a4feeb37a8271c06
chagy <chagy_x_@hotmail.com>
2022-03-20 14:04:42 +0700

    Seeder role & permission

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'employee']);
+        Role::create(['name' => 'customer']);
+
+        Permission::create(['name' => 'pos']);
+        Permission::create(['name' => 'import']);
+    }
+}
